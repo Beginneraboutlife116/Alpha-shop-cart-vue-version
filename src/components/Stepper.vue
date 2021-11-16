1bca4678c7730473001021adad696a0795adab2c
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-11-16 17:10:46 +0800

    feat/increase Stepper component

---
+++ src/components/Stepper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="stepper">
+    <span class="stepper__step-line line-active"></span>
+    <div class="stepper__step" v-for="(stepper, index) of steppers" :key="index">
+      <div class="stepper__step-circle">
+        <span class="stepper__step-circle--number"> {{index + 1}} </span>
+      </div>
+      <div class="stepper__step-title"> {{stepper}} </div>
+    </div>
+    <span class="stepper__step-line"></span>
+  </div>
+</template>
+
+<script>
+const stepperData = ['寄送地址', '運送方式', '付款資訊']
+
+export default {
+  name: 'Stepper',
+  data () {
+    return {
+      steppers: [...stepperData]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../scss/components/Stepper.scss';
+</style>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,14 +1,25 @@
 <template>
   <section>
-    <h1 class="checkout">結帳</h1>
-    <section class="content">
-      <!-- stepper -->
+    <h1 class="checkout-title">結帳</h1>
+    <section class="checkout-content">
+      <Stepper />
       <!-- form -->
       <!-- shoppingCart -->
       <!-- buttons -->
     </section>
   </section>
 </template>
+
+<script>
+import Stepper from '../components/Stepper.vue'
+
+export default {
+  name: 'Checkout',
+  components: {
+    Stepper
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../scss/layout/Checkout.scss';
